5923b99f8b9cd9b4f70c84c07299537cf82e9fc3
Jaflet3 <joeljaflet3905@gmail.com>
2026-07-13 21:52:03 +0530

    Create Footer.jsx

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+function Footer() {
+  return (
+    <footer>
+      <p>© 2026 Don Toys | Bringing Smiles with Every Toy 🎈</p>
+    </footer>
+  );
+}
+
+export default Footer;
